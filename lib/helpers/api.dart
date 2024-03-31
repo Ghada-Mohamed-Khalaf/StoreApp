5be2031f3e6f9b5e1cb92e6644b6c5d223b58a88
ghadaM2001 <ghm594733@gmail.com>
2024-03-31 04:08:39 -0700

    post request

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -12,5 +14,35 @@ class Api {
       throw Exception(
           "there is  a problem with status code ${response.statusCode}");
     }
+  }
+}
+
+Future<dynamic> post(
+    {required String uri,
+    @required dynamic body,
+    @required String? token}) async {
+  Map<String, String> header = {};
+  if (token != null) {
+    header.addAll({'Authorization': 'Bearer $token'});
+
+    http.Response response = await http.post(
+      Uri.parse(uri),
+      body: body,
+      headers: header,
+    );
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data;
+//     "title": 'test product',
+//     'price': '13.5',
+//     'description': 'lorem ipsum set',
+//     'image': 'https://i.pravatar.cc',
+//     'category': 'electronic',
+//   }, headers: {
+//     'Accept': 'applaction/json',
+//     'Content-Type': 'multipart/form-data',
+//   });
+// }
+// 'Authorization': 'Bearer' //after bearer add token
   }
 }
