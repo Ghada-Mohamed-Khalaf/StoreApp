e1308362f250cda523937f5de2994ed86efd7bf7
ghadaM2001 <ghm594733@gmail.com>
2024-03-31 07:01:18 -0700

    add put requset

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -15,24 +15,64 @@ class Api {
           "there is  a problem with status code ${response.statusCode}");
     }
   }
+
+  Future<dynamic> post(
+      {required String uri,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> header = {};
+    if (token != null) {
+      header.addAll({'Authorization': 'Bearer $token'});
+
+      http.Response response = await http.post(
+        Uri.parse(uri),
+        body: body,
+        headers: header,
+      );
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        return data;
+      } else {
+        throw Exception(
+            "there is  a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}");
+      }
+
+//     "title": 'test product',
+//     'price': '13.5',
+//     'description': 'lorem ipsum set',
+//     'image': 'https://i.pravatar.cc',
+//     'category': 'electronic',
+//   }, headers: {
+//     'Accept': 'applaction/json',
+//     'Content-Type': 'multipart/form-data',
+//   });
+// }
+// 'Authorization': 'Bearer' //after bearer add token
+    }
+  }
 }
 
-Future<dynamic> post(
+Future<dynamic> put(
     {required String uri,
     @required dynamic body,
     @required String? token}) async {
   Map<String, String> header = {};
   if (token != null) {
-    header.addAll({'Authorization': 'Bearer $token'});
+    header.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
 
     http.Response response = await http.post(
       Uri.parse(uri),
       body: body,
       headers: header,
     );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          "there is  a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}");
+    }
 
-    Map<String, dynamic> data = jsonDecode(response.body);
-    return data;
 //     "title": 'test product',
 //     'price': '13.5',
 //     'description': 'lorem ipsum set',
